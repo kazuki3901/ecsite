78f78c4f12f64de2448fa40b8484b30da81fbd07
kazu-0226 <k.k.5.2.26@i.softbank.jp>
2023-04-03 02:08:54 +0900

    Lesson_Ecsite

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/cotroller/Gson.java
@@ -0,0 +1,10 @@
+package jp.co.internous.ecsite.cotroller;
+
+public class Gson {
+
+	public String toJson(LoginDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ecsite/src/main/java/jp/co/internous/ecsite/cotroller/LoginDto.java
@@ -0,0 +1,19 @@
+package jp.co.internous.ecsite.cotroller;
+
+import jp.co.internous.ecsite.model.entity.User;
+
+public class LoginDto {
+
+	public LoginDto(int i, Object object, Object object2, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginDto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginDto(User user) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
